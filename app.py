a99c1d7dd45953c35504f3ed167faaad8b494483
Ryan Krohne <rkrohnesoccer@gmail.com>
2025-03-29 16:19:13 -0400

    ai api

--- app.py
+++ app.py
@@ -6,5 +6,12 @@ app = Flask(__name__)
 def health():
     return "API is healthy!", 200
 
+
+
+@app.route('/AI', methods=['GET'])
+def AI():
+    return "AI API is healthy!", 200
+
+
 if __name__ == "__main__":
     app.run(debug=True)
